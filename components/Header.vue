5dcc3875142f57ab17eebd97e53009e21c2b59ac
Kevin Britten <kevinjbritten@gmail.com>
2023-11-14 16:26:15 -0500

    add header and tailwind

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="bg-blue-500 p-4">
+    <div class="container mx-auto">
+      <h1 class="text-white text-2xl font-bold">My Awesome Header</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  // Component logic here
+};
+</script>
+
+<style scoped>
+/* Component-specific styles here */
+</style>
